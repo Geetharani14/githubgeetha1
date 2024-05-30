c23d16493466beb87c9653df0bc075e273535016
Geetharani14 <grani0458@gmail.com>
2024-05-30 15:38:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p $DOCKER_PASSWORD
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker login -u  geethadoclearn-p $DOCKER_PASSWORD
+    docker tag test geethadoclearn/dev
+    docker push  geethadoclearn/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p $DOCKER_PASSWORD
-    docker tag test ar8888/prod 
-    docker push ar8888/prod
+    docker login -u  geethadoclearn -p $DOCKER_PASSWORD
+    docker tag test  geethadoclearn/prod 
+    docker push  geethadoclearn/prod
 fi
